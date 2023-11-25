f4fcdd6d038d7f631474ae0a68df9e519f00d816
eTonguzov <etonguzov@gmail.com>
2023-11-25 19:28:49 +0300

    25.11.23

---
+++ 06. Операторы стравнения/задача 1.js
@@ -0,0 +1,9 @@
+//Каким будет результат этих выражений?
+
+console.log(5 > 4)// true
+console.log("ананас" > "яблоко")//false
+console.log("2" > "12")//true
+console.log(undefined == null)//true тк не строгое
+console.log(undefined === null) //false тк строгое
+console.log(null == "\n0\n")//false "\n0\n" - новая строка ноль и новая строка
+console.log(null === +"\n0\n")//false
